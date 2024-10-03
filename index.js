743db6684c853c6febe2af2382e10b7f021ce958
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-10-03 21:01:10 +0300

    Fix comma and semi rules

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ module.exports = {
     'sort-destructure-keys'
   ],
   rules: {
+    'comma-dangle': ['error', 'never'],
     'import/order': ['error', { alphabetize: { order: 'asc' } }],
     'object-curly-newline': ['error', { consistent: true, multiline: true }],
+    'semi': ['error', 'never'],
     'sort-destructure-keys/sort-destructure-keys': 'error',
     'sort-imports': ['error', { ignoreDeclarationSort: true }],
     'sort-keys': ['error', 'asc', { natural: true }]
