4447e151e4b55cbc0d700e9ca6c3dbc230790720
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-10-07 06:53:03 +0300

    Disable no-await-in-loop rule

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 module.exports = {
-  extends: [
-    'airbnb-base'
-  ],
+  extends: 'airbnb-base',
   plugins: [
     'sort-destructure-keys'
   ],
   rules: {
     'comma-dangle': ['error', 'never'],
     'import/order': ['error', { alphabetize: { order: 'asc' } }],
+    'no-await-in-loop': 'off',
     'object-curly-newline': ['error', { consistent: true, multiline: true }],
-    'semi': ['error', 'never'],
+    semi: ['error', 'never'],
     'sort-destructure-keys/sort-destructure-keys': 'error',
     'sort-imports': ['error', { ignoreDeclarationSort: true }],
     'sort-keys': ['error', 'asc', { natural: true }]
